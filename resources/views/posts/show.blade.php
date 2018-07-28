ab195111988ccd4e0530b5b2903b29962d569136
hesamsameni <hesam.sameni@gmail.com>
2018-07-28 11:02:51 +0430

    Comment Model And Migration

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,26 @@
-@extends ('Layouts/master')
-
-@section ('content')
+@extends ('Layouts/master') @section ('content')
 <div class="col-sm-9 blog-main" style="padding-top:50px">
-<div class="blog-post">
-    <h2 class="blog-post-title">{{$post->title}}</h2>
-    <p class="blog-post-meta"><span class="postCategory">{{$post->category}}</span>{{$post->created_at -> toFormattedDateString()}}</p>
-    <p>{{$post->body}}</p>
-</div>
+    <div class="blog-post">
+        <h2 class="blog-post-title">{{$post->title}}</h2>
+        <p class="blog-post-meta"><span class="postCategory">{{$post->category}}</span>{{$post->created_at -> toFormattedDateString()}}</p>
+        <p>{{$post->body}}</p>
+    </div>
+    <hr>
+    <div class="comments">
+        <ul class="list-group">
+            <li class="list-group-item">
+                @foreach ($post->comments as $comment)
+                <strong>
+{{ $comment->created_at->diffForHumans() }}
+</strong>
+                <article>
+                    {{ $comment->body}}
+                    <span class="username">{{ $comment -> username }}</span>
+                </article>
+                @endforeach
+            </li>
+        </ul>
+
+    </div>
 </div>
 @endsection
-
-
-
